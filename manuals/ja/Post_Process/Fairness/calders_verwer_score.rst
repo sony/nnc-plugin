29d567977950c61ceac9e47620b68fb36a163003
Kenji Suzuki <66014105+Kenji-Suzuki-S@users.noreply.github.com>
2022-10-13 19:41:22 +0900

    [Fairness] Added Fairness plot feature in CV Score plug-in output (#57)
    * first commit
    
    * first commit
    
    * edit URL
    
    * first commit
    
    * Update Representerpoint.rst
    
    * edit copyright
    
    * format copyright
    
    * byte to sting
    
    * Added fairness plot in NNC plug-in output
    
    Co-authored-by: matsun715 <Naoki.Matsunaga@sony.com>
    Co-authored-by: matsun715 <102739906+matsun715@users.noreply.github.com>
    Co-authored-by: Dileep Panigrahi <dileep.panigrahi1@sony.com>

--- manuals/ja/Post_Process/Fairness/calders_verwer_score.rst
+++ manuals/ja/Post_Process/Fairness/calders_verwer_score.rst
@@ -1,9 +1,9 @@
 Fairness/CV Score (Tabular)
 ~~~~~~~~~~~~~~~~~~~~~~
 CV scoreは表データに対するモデルの公平さを評価する指標です．
-バイアスが現れる可能性のある属性(年齢や性別など)間で陽性と予測したデータの差を計算します．
+バイアスが現れる可能性のある属性において多数派は'privileged'クラスと呼ばれ，バイアスが現れる可能性のある属性において少数派は'unprivileged'クラスと呼ばれます． 
+'unprivileged'クラスと'privileged'クラスの2つのグループ間で陽性と予測したデータの差を計算します．
 `Fairness-aware classifier with prejudice remover regularizer. Toshihiro Kamishima, Shotaro Akaho, Hideki Asoh & Jun Sakuma.Joint European Conference on Machine Learning and Knowledge Discovery in Databases ECML PKDD 2012: Machine Learning and Knowledge Discovery in Databases pp 35–50 <https://link.springer.com/chapter/10.1007/978-3-642-33486-3_3>`_
-
 
 
 Input Information
@@ -20,18 +20,24 @@ Input Information
    * - input
      - CV scoreが計算されるデータを含むCSVファイルを指定します．CVスコアを計算し，"評価"タブに表示される結果を出力するには，デフォルトの output_result.csv を使用します．
 
-   * - label_variable
-     - inputで指定したCSVファイルから，正解ラベルとして使用する列名を指定します．
+   * - target_variable
+     - inputで指定したCSVファイルから，ターゲットラベルとして使用する列名を指定します．
 
    * - output_variable
-     - inputで指定したCSVファイルから，モデル出力として使用する列名を指定します．(クラス分類の出力など．)
+     - inputで指定したCSVファイルから，モデル出力として使用する列名を指定します．(クラス分類の出力など．デフォルトでは"y'"が指定されます．)
 
    * - privileged_variable
-     - inputで指定したCSVファイルから，バイアスが現れる可能性のある属性において保護されている属性として使用する列名を指定します．バイアスが現れる可能性のある属性において多数派は'privileged'クラスと呼ばれます．デフォルトでは，'female'がprivileged_variableに割り当てられます．
+     - inputで指定したCSVファイルから，'privileged'属性として使用する列名を指定します．(バイアスが現れる可能性のある属性において多数派は'privileged'クラスと呼ばれます．)
 
    * - unprivileged_variable
-     - inputで指定したCSVファイルから，バイアスが現れる可能性のある属性において保護されていない属性として使用する列名を指定します．バイアスが現れる可能性のある属性において少数派は'unprivileged'クラスと呼ばれます．デフォルトでは，'male'がunprivileged_variableに割り当てられます．
+     - inputで指定したCSVファイルから，'unprivileged'属性として使用する列名を指定します． (バイアスが現れる可能性のある属性において少数派は'unprivileged'クラスと呼ばれます．)
 
+   * - clf_threshold
+     - 最適なクラス分類しきい値を指定します．デフォルトのしきい値は 0.5 です．
+
+   * - fair_threshold
+     - 公平性のしきい値を決定します．デフォルト値は0.10です．したがって，0.0と0.1の間のすべての結果は"公平"であり，範囲外は"不公平"であると確認することができます．
+  
    * - num_samples
      - CV scoreを計算するサンブル数を指定します．デフォルトでは'all'が設定されており，入力ファイルの全てのサンプルでCVスコアを計算します．
 
@@ -48,21 +54,9 @@ CSVファイルの列の情報は下記の通りです．
    :widths: 30 70
    :class: longtable
 
-   * - Object Variable
-     - CV score計算用に指定された正解ラベル.
-
-   * - Output variable
-     - CV score計算用に指定されたモデルの出力.
-
-   * - Privileged variable
-     - 指定されたバイアスが現れる可能性のある属性において保護されている属性．
-
-   * - Unprivileged variable
-     - 指定されたバイアスが現れる可能性のある属性において保護されていない属性．
-
-   * - Number of samples
-     - CV score計算に使用されたサンプル数．
-
+   * - Fairness Plot
+     - Fairness Plot列では，'privileged'属性と'unprivileged'属性間の公平性を図として確認できます．棒グラフが緑のエリアからはみ出している場合は，公平性の指標が満たされていないことを意味します．
+   
    * - CV score
      - CV scoreの値．低いほど公平なモデルであることを意味します．
 
--- plugins/_Post_Process/_Fairness/calders_verwer_score.py
+++ plugins/_Post_Process/_Fairness/calders_verwer_score.py
@@ -13,9 +13,62 @@
 # limitations under the License.
 import argparse
 import csv
+import os
+import sys
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from nnabla import logger
+
+
+def plot_fairness(model_fairness, args,
+                  ideal_fairness=[0, 0.10],
+                  metric="CV Score", file_path=None):
+    """
+    graphical visualization of fairness of the model
+    Args:
+        model_fairness (float) : fairness of the model.
+        args (dict): arguments it requires.
+        ideal_fairness (list) : model ideal fairness range.
+        metric (str) : name of the fairness metric.
+        file_path (str) : location of the file, where the fairness plot to be saved
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5), sharey=True)
+    ax.set_ylim([0.0, 1])
+    ax.axhline(y=0.0, color='b', linestyle='-')
+    ax.bar(['Fairness'], [model_fairness], color="darkcyan", width=2)
+    ax.axhspan(ideal_fairness[0], ideal_fairness[1],
+               facecolor='0.5', color="lightgreen", alpha=0.5)
+    ax.set_ylabel(metric)
+    fig.text(0.92, 0.7, '\n'.join(
+        [f"Privileged Variable : {args.privileged_variable} \n"
+         f"Unprivileged Variable : {args.unprivileged_variable} \n"
+         f"Total number of samples : {args.num_samples} \n"]),
+        fontsize='15')
+    if ideal_fairness[0] < model_fairness < ideal_fairness[1]:
+        ax.text(1.11, 0, "Fair", fontweight='bold', color='Green')
+        ax.text(1.11, 0.5, "Bias", fontweight='bold', color='darkgray')
+        ax.text(0, model_fairness + 0.05,
+                str(round(model_fairness, 3)), fontweight='bold',
+                color='Green', bbox=dict(facecolor='red', alpha=0.3))
+        fig.text(0.92, 0.6, '\n'.join(
+            [f"\nFairness :\n{metric} :  {model_fairness:.3f}"]), fontsize='15')
+        fig.text(0.92, 0.55, '\n'.join(
+            [f"(Model is Fair)"]), color='Green', fontweight='bold', fontsize='12')
+    else:
+        ax.text(1.11, 0, "Fair", fontweight='bold', color='darkgray')
+        ax.text(1.11, 0.5, "Bias", fontweight='bold', color='red')
+        ax.text(0, model_fairness + 0.05,
+                str(round(model_fairness, 3)), fontweight='bold',
+                color='red', bbox=dict(facecolor='red', alpha=0.3))
+        fig.text(0.92, 0.6, '\n'.join(
+            [f"\nFairness :\n{metric} :  {model_fairness:.3f}"]), fontsize='15')
+        fig.text(0.92, 0.55, '\n'.join(
+            [f"(Model is Biased)"]), color='red', fontweight='bold', fontsize='12')
+    fig.text(0.92, 0.4, '\n'.join(
+        [f"Fairness for this metric between {ideal_fairness[0]} and {ideal_fairness[1]}\n"]), fontsize='15')
+    fig.suptitle(metric, fontsize=15)
+    plt.savefig(file_path, bbox_inches='tight')
 
 
 def calders_verwer_score(output_f, output_m, classification_threshold):
@@ -79,7 +132,7 @@ def func(args):
             logger.log(99, '{} : {}'.format(col, name))
         return col
 
-    label_col = enum_cols('label_variable', args.label_variable)
+    label_col = enum_cols('target-variable', args.target_variable)
     output_col = enum_cols('output-variable', args.output_variable)
     privileged_col = enum_cols('privileged_variable', args.privileged_variable)
     unprivileged_col = enum_cols(
@@ -93,22 +146,39 @@ def func(args):
     clf_out = np.asarray(extract_col(output_col)).flatten()
     privileged_variable = np.array(extract_col(privileged_col))
     unprivileged_variable = np.array(extract_col(unprivileged_col))
+    # check privileged/unprivileged variables
+    if not ((privileged_variable == 1.0) == (unprivileged_variable == 0.0)).all():
+        logger.log(
+            99, "Both privileged and unprivileged variable values should not be same")
+        sys.exit(0)
+
     out_privileged = clf_out[privileged_variable == 1.0]
     out_unprivileged = clf_out[unprivileged_variable == 1.0]
-    classification_threshold = 0.5  # default optimal classification threshold
 
-    preds = np.where(clf_out > classification_threshold, 1, 0)
+    fair_flag = abs(args.fair_threshold) <= 1  # check the fairness value
+    if not fair_flag:
+        logger.error(f'Fairness value out of range! '
+                     f'Fairness of this metric is between 0 and 1.0 , Please enter again', exc_info=0)
+        sys.exit(0)
+    else:
+        logger.log(99,
+                   f'Fairness of this metric is between 0 and {-abs(args.fair_threshold)}')
+        ideal_fairness = [0, abs(args.fair_threshold)]
+        metric = "CV Score"
+        file_path = os.path.join(os.path.dirname(
+            os.path.abspath(args.output)), metric + '.png')
+
+    preds = np.where(clf_out > args.clf_threshold, 1, 0)
     logger.log(99, type(preds))
     cv_score = calders_verwer_score(
-        out_privileged, out_unprivileged, classification_threshold)
+        out_privileged, out_unprivileged, args.clf_threshold)
     clf_accuracy = accuracy_score(y_actual, preds)
+    plot_fairness(cv_score, args, metric=metric,
+                  ideal_fairness=ideal_fairness, file_path=file_path)
     with open(args.output, 'w', newline="\n", encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['Object Variable', 'Output variable', 'Privileged variable', 'Unprivileged variable',
-                         'Number of samples', 'CV score', 'Accuracy'])
-        writer.writerow([args.label_variable, args.output_variable,
-                        args.privileged_variable, args.unprivileged_variable,
-                        len(in_table), cv_score, clf_accuracy])
+        writer.writerow(['Fairness Plot', 'CV Score', 'Accuracy'])
+        writer.writerow([file_path, cv_score, clf_accuracy])
 
 
 def main():
@@ -125,30 +195,38 @@ def main():
         help='path to input csv file, output result shown in the evaluation tab (csv) default=output_result.csv',
         required=True, default='output_result.csv')
     parser.add_argument(
-        '-l', '--label_variable', help='specify the label variable (variable) default=y__0:label',
-        required=True, default='y__0:label')
+        '-t', '--target_variable',
+        help='specify the actual target(label) variable (variable)',
+        required=True)
     parser.add_argument(
         '-p', '--output_variable',
-        help='specify the output variable, in this classification o/p (variable) default=Sigmoid',
-        required=True, default='Sigmoid')
+        help="specify the predicted output variable, expect sigmoid output (variable) default=y'",
+        required=True, default="y'")
     parser.add_argument(
         '-zp', '--privileged_variable',
         help='specify the privileged variable which to compute the '
-             'discrimination score (variable) default=z__0:sex=Female',
-        required=True, default='z__0:sex=Female')
+             'discrimination score (variable)',
+        required=True)
     parser.add_argument(
         '-zu', '--unprivileged_variable',
         help='specify the unprivileged variable which to compute the '
-             'discrimination score (variable) default=z__1:sex=Male',
-        required=True, default='z__1:sex=Male')
-
+             'discrimination score (variable)',
+        required=True)
+    parser.add_argument(
+        '-th', '--clf_threshold',
+        help='best optimal classification threshold, default=0.5',
+        required=True, type=float, default=0.5)
+    parser.add_argument(
+        '-fair_th',
+        '--fair_threshold',
+        help='Specify fairness threshold, default value is 0.10, default=0.10',
+        type=float, default=0.10)
     parser.add_argument(
         '-n', '--num_samples',
-        help='number of samples N, '
-             'if num_samples = all, it will take all the samples from the input csv file (variable), default=all',
+        help='number of samples N (variable), default=all',
         required=True, default='all')
     parser.add_argument(
-        '-o', '--output', help='path to output file (csv) default=fairness.csv', required=True, default='fairness.csv')
+        '-o', '--output', help='path to output file (csv) default=cv_score.csv', required=True, default='cv_score.csv')
     parser.set_defaults(func=func)
     args = parser.parse_args()
     args.func(args)
